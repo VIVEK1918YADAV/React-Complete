3d805d3c5428df7d1eee070af423cad103d525a1
VIVEK1918YADAV <vivekyadav86706@gmail.com>
2023-08-25 13:01:50 +0530

    first

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * create nested html structure inside your react application  
+ * 
+ * <div id ="parent">
+ *    <div id = "child">
+ *       <h1> I am H1 tag </h1>
+ *       <h2> I am H2 tag </h2>
+ *    </div>
+ * <div id="child2">
+ * <h1> I am H1 tag </h1>
+ * <h2> I am H2 tag </h2>
+ * </div>
+ * </div>
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+const parent =  React.createElement("div",  {id: "parent" }, [// (react element)parent is not a html or any tag it is an object
+    
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+    ]), //3rd argument
+
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+]);
+
+//JSX  
+
+// const heading =  React.createElement(
+//     "h1", 
+//     {id: "heading", xyz: "abc"},
+//      "Hello World from React!"
+//      );
+
+console.log(parent);//object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
